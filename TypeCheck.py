6ac024465eb51c224d3dd2feb5a6990f27221c4a
Sateswar Sahu <sateswar_sahu@hotmail.com>
2022-11-26 21:34:49 +0530

    sum all the digits of a number

--- TypeCheck.py
+++ TypeCheck.py
@@ -1,3 +1,6 @@
 numberOfCharacters=len(input("Name : "))
-print(type(numberOfCharacters))
+#numberOfCharacters=(input("Name : "))
+
+if type(numberOfCharacters) is int: print("Your Name has "+str(numberOfCharacters)+" characters")
+else:print("Input parameter is not a number, please enter a number")
 #print("Your Name has "+ numberOfCharacters+" characters")
