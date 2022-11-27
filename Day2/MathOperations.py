f7a67c0628f6fc1699302a257a283b52ced39055
Sateswar Sahu <sateswar_sahu@hotmail.com>
2022-11-27 21:52:26 +0530

    Day2

---
+++ Day2/MathOperations.py
@@ -0,0 +1,16 @@
+
+print(4+3*2-2)
+
+#PEMDAS (Ba Ra Ha Gu Mi Fe) Left to right
+# P()
+# E 
+# MD */
+# AS +-
+
+########BMI Calculation ###### BMI=Weight(KG)/Heigh exponential of 2(M2)
+weight=input("Weight in KG: ")
+height=input("Height in meter : ")
+
+Bmi=int(weight)/float(height)**2
+
+print("BMI "+ str(Bmi))
---
+++ Day2/fString.py
@@ -0,0 +1,6 @@
+name ='Sateswar sahu'
+age=36
+height=5.5
+boolean= True
+
+print(f'Your Name is {name}, age is {age}, height is {height} and boolean {boolean}' )
